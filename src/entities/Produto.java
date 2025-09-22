f2675d08fe9d39602493960d23ec4e58d732066b
walter dev reis <walter-reis@outlook.com>
2025-09-22 15:46:59 -0300

    Criada e testada a classe Produto

---
+++ src/entities/Produto.java
@@ -0,0 +1,56 @@
+package entities;
+
+import java.util.Objects;
+
+public class Produto {
+
+    private long cod;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(long cod, String nome, double preco, int quantidade) {
+        this.cod = cod;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public long getCod() {
+        return cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Double.compare(preco, produto.preco) == 0 && Objects.equals(nome, produto.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, preco);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "cod=" + cod +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
